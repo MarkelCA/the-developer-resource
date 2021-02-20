3228250d7217ac2b4348764f8ba772e9346b35fb
MarkelCA <cuestaarribas.markel@gmail.com>
2021-02-20 02:02:46 +0100

    intalled Tailwind and styling Article.css

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import resources from '../resources-list.json';
-import tachyons from 'tachyons';
 import templateImg from '../img/300.png';
+import './Article.css';
 class Article extends Component {
     constructor() {
         super();
@@ -10,8 +10,8 @@ class Article extends Component {
         const { id } = this.props;
         const { name, description, href} = resources[id];
         return (
-            <div className="article center  w-70-ns bg-light-blue pa3 ma2 shadow-5">
-                <img src={templateImg} alt="template-image" />
+            <div className="article bg-blue-400 m-4 flex flex-col justify-center">
+                <img src={templateImg} alt="template-image" className='' />
                 <div className="resource-info " >
                     <h3>{name}</h3>
                     <p>{description}</p>
